31690995358135127e916376b7e7ac83689ffc17
Mark Hanly <m.hanly@unsw.edu.au>
2023-04-19 12:08:42 +1000

    Added data folder and puzzle icons

--- R/app_server.R
+++ R/app_server.R
@@ -6,25 +6,7 @@
 #' @noRd
 app_server <- function(input, output, session) {
 
-  # Read the raw data
-
- rankings <- c('Unknown', 'Unlikely', 'Limited', 'Potential', 'Possible', 'Likely')
-
-  menuData <- read.csv("data-raw/menu-data.csv") %>%
-    dplyr::mutate(
-      alcohol = factor(alcohol, levels=0:5, labels=rankings),
-      tobacco = factor(tobacco, levels=0:5, labels=rankings),
-      cannabis = factor(cannabis, levels=0:5, labels=rankings),
-      other = factor(other, levels=0:5, labels=rankings),
-      alcoholUse = factor(alcoholUse, levels=0:5, labels=rankings),
-      alcoholBehav = factor(alcoholBehav, levels=0:5, labels=rankings),
-      alcoholHarm = factor(alcoholHarm, levels=0:5, labels=rankings),
-      tobaccoUse = factor(tobaccoUse, levels=0:5, labels=rankings),
-      tobaccoBehav = factor(tobaccoBehav, levels=0:5, labels=rankings),
-      tobaccoQuit = factor(tobaccoQuit, levels=0:5, labels=rankings),
-      cannabisUse = factor(cannabisUse, levels=0:5, labels=rankings),
-      cannabisBehav = factor(cannabisBehav, levels=0:5, labels=rankings)
-      )
+  rankings <- c('Unknown', 'Unlikely', 'Limited', 'Potential', 'Possible', 'Likely')
 
   # Define colors
   colors <- data.frame(ranking = rankings,
